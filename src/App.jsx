eca434c327a7f5b922d1a2abf146cfaeb0706f63
KEITH ANGELO TUMANENG <katumaneng24@gmail.com>
2024-09-12 04:42:57 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Login from "./pages/login";
+import UserProfile from "./pages/userProfile";
+import "../src/assets/css/index.css";
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <UserProfile />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ".././assets/css/login.css";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+const Login = () => {
+  return (
+    <>
+      <div className="container-fluid my-auto gradient-form">
+        <div className="row justify-content-center align-items-center min-vh-100">
+          <div className="card d-flex justify-content-center align-items-center w-50">
+            <div className="card-body d-flex flex-column">
+              <div className="text-center">
+                <ApartmentIcon sx={{ fontSize: 90 }} />
+                <h4 className="mt-1 mb-5 pb-1">Tremor Track</h4>
+              </div>
+
+              <p>Please login to your account</p>
+
+              <div className="mb-4">
+                <label htmlFor="form1">Email address</label>
+                <input id="form1" type="email" className="form-control" />
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="form2">Password</label>
+                <input id="form2" type="password" className="form-control" />
+              </div>
+
+              <div className="text-center pt-1 mb-5 pb-1">
+                <button className="mb-4 w-100 gradient-custom-2 btn btn-primary">
+                  Sign in
+                </button>
+                <a className="text-muted" href="#!">
+                  Forgot password?
+                </a>
+              </div>
+
+              <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+                <p className="mb-0">Don't have an account?</p>
+                <button className="mx-2 btn btn-outline-danger">Sign up</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;

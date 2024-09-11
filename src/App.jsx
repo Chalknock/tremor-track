bd6e48e3a271235c662c112a12cc884b5f921b79
KEITH ANGELO TUMANENG <katumaneng24@gmail.com>
2024-09-12 05:23:43 +0800

    add material icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Login from "./pages/login";
 import UserProfile from "./pages/userProfile";
+import Sidebar from "./base/sidebar";
 import "../src/assets/css/index.css";
 
 function App() {
   return (
     <>
       {/* <Login /> */}
-      <UserProfile />
+      {/* <UserProfile /> */}
+      <Sidebar />
     </>
   );
 }
---
+++ src/base/sidebar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./sidebar.css";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+const sidebar = () => {
+  const [currImg, setCurrImg] = useState(0);
+  const images = [
+    { title: "San Diego", subtitle: "This is San Diego" },
+    {
+      title: "Salvador, Brazil",
+      subtitle: "The Best City in the World",
+    },
+    {
+      title: "UBC (Vancouver)",
+      subtitle: "The University of British Columbia",
+    },
+  ];
+  return (
+    <div className="carousel">
+      <div className="carouselInner">
+        asdsda
+        <div
+          className="left"
+          onClick={() => {
+            currImg > 0 && setCurrImg(currImg - 1);
+          }}
+        >
+          <ArrowBackIosIcon style={{ fontSize: 30 }} />
+        </div>
+        <div className="center">
+          <h1>{images[currImg].title}</h1>
+          <p>{images[currImg].subtitle}</p>
+        </div>
+        <div
+          className="right"
+          onClick={() => {
+            currImg < images.length - 1 && setCurrImg(currImg + 1);
+          }}
+        >
+          <ArrowForwardIosIcon style={{ fontSize: 30 }} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default sidebar;

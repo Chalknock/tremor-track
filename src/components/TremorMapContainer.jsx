8e2b1e469e88cb33ab3775a85c28b46211039163
Chalknock <katumaneng24@gmail.com>
2024-10-21 01:12:01 +0800

    try to fix the icon for map marker

--- src/components/TremorMapContainer.jsx
+++ src/components/TremorMapContainer.jsx
@@ -1,15 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-// Set default icon options for markers
-delete L.Icon.Default.prototype._getIconUrl; // Remove default icon URL method
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+import L from "leaflet"; // Ensure you import Leaflet
 
 const TremorMapContainer = ({ formData, updateCoordinates }) => {
   const ClickHandler = ({ setMarkerPosition, updateCoordinates }) => {
@@ -55,42 +47,61 @@ const TremorMapContainer = ({ formData, updateCoordinates }) => {
   }, []);
 
   useEffect(() => {
-    if (!isNaN(formData.lat) && !isNaN(formData.lon)) {
-      setMapCenter([formData.lat, formData.lon]);
-      setMarkerPosition([formData.lat, formData.lon]);
-    } else if (location.lat && location.lon) {
-      setMapCenter([location.lat, location.lon]);
+    let initialPosition = [formData.lat, formData.lon];
+
+    if (isNaN(initialPosition[0]) || isNaN(initialPosition[1])) {
+      if (location.lat && location.lon) {
+        setMapCenter([location.lat, location.lon]);
+      }
+    } else {
+      setMapCenter(initialPosition);
+      setMarkerPosition(initialPosition);
     }
-  }, [formData, location]);
+  }, [formData, location, updateCoordinates]);
+
+  // Create a custom marker icon using the Material UI icon
+  const customIcon = new L.Icon({
+    iconUrl:
+      "data:image/svg+xml;utf8," +
+      encodeURIComponent(
+        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <g fill="none" stroke="currentColor" stroke-width="2">
+          <path d="M12 2C8.134 2 5 5.134 5 9c0 3.8 7 11 7 11s7-7.2 7-11c0-3.866-3.134-7-7-7z"/>
+          <circle cx="12" cy="9" r="3"/>
+        </g>
+      </svg>`
+      ),
+    iconSize: [24, 24], // Size of the icon
+    iconAnchor: [12, 24], // Anchor point
+  });
 
   return (
-    <MapContainer
-      center={
-        isNaN(markerPosition[0]) || isNaN(markerPosition[1])
-          ? mapCenter
-          : markerPosition
-      }
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "45vh", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ClickHandler
-        setMarkerPosition={setMarkerPosition}
-        updateCoordinates={updateCoordinates}
-      />
-      {markerPosition && (
-        <Marker position={markerPosition}>
-          <Popup>
-            Latitude: {markerPosition[0]} <br />
-            Longitude: {markerPosition[1]}
-          </Popup>
-        </Marker>
-      )}
-    </MapContainer>
+    <>
+      <MapContainer
+        center={mapCenter}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "45vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ClickHandler
+          setMarkerPosition={setMarkerPosition}
+          updateCoordinates={updateCoordinates}
+        />
+
+        {markerPosition && (
+          <Marker position={markerPosition} icon={customIcon}>
+            <Popup>
+              Latitude: {markerPosition[0]} <br />
+              Longitude: {markerPosition[1]}
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
+    </>
   );
 };
 

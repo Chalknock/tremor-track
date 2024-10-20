fd7d22beff51a057d48d3eaa6f8f593f35bafef2
Chalknock <katumaneng24@gmail.com>
2024-10-21 01:23:55 +0800

    try to fix the icon for map marker

--- src/components/TremorMapContainer.jsx
+++ src/components/TremorMapContainer.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import PersonPinIcon from "@mui/icons-material/PersonPin";
-
-const ClickHandler = ({ setMarkerPosition, updateCoordinates }) => {
-  const map = useMap();
-
-  const handleClick = (event) => {
-    const { lat, lng } = event.latlng;
-    updateCoordinates(lat, lng);
-    setMarkerPosition([lat, lng]);
-  };
-
-  useEffect(() => {
-    map.on("click", handleClick);
-    return () => {
-      map.off("click", handleClick);
-    };
-  }, [map, setMarkerPosition, updateCoordinates]);
-
-  return null;
-};
 
 const TremorMapContainer = ({ formData, updateCoordinates }) => {
-  const [location, setLocation] = useState({ lat: null, lon: null });
-  const [markerPosition, setMarkerPosition] = useState([14.599512, 120.984222]);
-  const [mapCenter, setMapCenter] = useState([14.599512, 120.984222]);
+  const ClickHandler = ({ setMarkerPosition, updateCoordinates }) => {
+    const map = useMap();
+
+    const handleClick = (event) => {
+      const { lat, lng } = event.latlng;
+      updateCoordinates(lat, lng);
+      setMarkerPosition([lat, lng]);
+    };
+
+    useEffect(() => {
+      map.on("click", handleClick);
+      return () => {
+        map.off("click", handleClick);
+      };
+    }, [map, setMarkerPosition, updateCoordinates]);
+
+    useEffect(() => {
+      map.setView(mapCenter);
+    }, [map, mapCenter]);
+    return null;
+  };
 
   useEffect(() => {
     const handleSuccess = (position) => {
@@ -45,48 +43,52 @@ const TremorMapContainer = ({ formData, updateCoordinates }) => {
     }
   }, []);
 
+  let initialPosition = [formData.lat, formData.lon];
+
+  const [location, setLocation] = useState({ lat: null, lon: null });
+  const [markerPosition, setMarkerPosition] = useState([14.599512, 120.984222]);
+  const [mapCenter, setMapCenter] = useState([14.599512, 120.984222]);
   useEffect(() => {
-    if (!isNaN(formData.lat) && !isNaN(formData.lon)) {
+    if (isNaN(formData.lat) || isNaN(formData.lon)) {
+      if (location.lat && location.lon) {
+        setMapCenter([location.lat, location.lon]);
+      }
+    } else {
       setMapCenter([formData.lat, formData.lon]);
       setMarkerPosition([formData.lat, formData.lon]);
-    } else if (location.lat && location.lon) {
-      setMapCenter([location.lat, location.lon]);
     }
-  }, [formData, location]);
-
+  }, [formData, location, updateCoordinates]);
   return (
-    <MapContainer
-      center={
-        isNaN(formData.lat) || isNaN(formData.lon)
-          ? mapCenter
-          : [formData.lat, formData.lon]
-      }
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "45vh", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ClickHandler
-        setMarkerPosition={setMarkerPosition}
-        updateCoordinates={updateCoordinates}
-      />
-      {markerPosition && (
-        <Marker position={markerPosition}>
-          <Popup>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <PersonPinIcon style={{ marginRight: "8px" }} />
-              <div>
-                Latitude: {markerPosition[0]} <br />
-                Longitude: {markerPosition[1]}
-              </div>
-            </div>
-          </Popup>
-        </Marker>
-      )}
-    </MapContainer>
+    <>
+      <MapContainer
+        center={
+          isNaN(initialPosition[1], isNaN(initialPosition[0]))
+            ? mapCenter
+            : initialPosition
+        }
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "45vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ClickHandler
+          setMarkerPosition={setMarkerPosition}
+          updateCoordinates={updateCoordinates}
+        />
+
+        {markerPosition && (
+          <Marker position={markerPosition}>
+            <Popup>
+              Latitude: {markerPosition[0]} <br />
+              Longitude: {markerPosition[1]}
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
+    </>
   );
 };
 
